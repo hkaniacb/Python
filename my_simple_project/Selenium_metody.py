9895e7558c8c6ec5a7158a3bb3762c15d96b1de9
Hubert <54799196+hkaniacb@users.noreply.github.com>
2019-10-02 13:46:56 +0200

    Update Selenium_metody.py

--- my_simple_project/Selenium_metody.py
+++ my_simple_project/Selenium_metody.py
@@ -1,1 +1,2 @@
-self.driver.find_element_by_xpath("//h2[contains(text(),'O sprzedającym')]")  # szukanie obiektu zawierająego jakiś tekst 
+driver.find_element_by_xpath("//h2[contains(text(),'O sprzedającym')]")  # szukanie obiektu odpowiedniej frazy tekstcie
+driver.find_element_by_xpath("//h2[contains(.,'SampleText')]") # szukanie w inner tekscie występującej frazy w obiekcie 
